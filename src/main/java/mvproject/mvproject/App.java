92bc3fda85b05ee96fcbc617561030fa4129ecaf
zhelyazkoi <zhelyazko.ivanov@playtech.com>
2016-01-02 12:53:24 +0200

    Initial commit

---
+++ src/main/java/mvproject/mvproject/App.java
@@ -0,0 +1,13 @@
+package mvproject.mvproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
